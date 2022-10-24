93ab8948dfbfd8906af5b17a525bff5871798b3e
suhye0n <contact.suhyeon@gmail.com>
2022-10-24 13:29:13 +0900

    2022.10.24 button name 변경

--- Week07WinFormsCounter/Form1.Designer.cs
+++ Week07WinFormsCounter/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnPress = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPress
+            // 
+            this.btnPress.Location = new System.Drawing.Point(379, 108);
+            this.btnPress.Name = "btnPress";
+            this.btnPress.Size = new System.Drawing.Size(94, 29);
+            this.btnPress.TabIndex = 0;
+            this.btnPress.Text = "button1";
+            this.btnPress.UseVisualStyleBackColor = true;
+            this.btnPress.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPress);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnPress;
     }
 }
--- Week07WinFormsCounter/Form1.cs
+++ Week07WinFormsCounter/Form1.cs
@@ -6,5 +6,10 @@ namespace Week07WinFormsCounter
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
